459d5df0ea92e53aa6e75c61f4e90e773d8b94a6
jbhouse <jbmoorhouse1@gmail.com>
2025-03-11 20:15:41 -0600

    oas-integration-test: Added @SpringBootTest e2e tests that leverage wiremock embedded server

--- services/src/main/java/com/moorhouse/SampleServiceImpl.java
+++ services/src/main/java/com/moorhouse/SampleServiceImpl.java
@@ -8,11 +8,11 @@ public class SampleServiceImpl implements SampleService {
   @Override
   public String getSampleResponse() {
     String externalResponse = externalApi.getSampleResponse();
-    return "sample response " + externalResponse;
+    return "sample response: " + externalResponse;
   }
   @Override
   public String postSampleResponse(String body) {
     String externalResponse = externalApi.postSampleResponse(body);
-    return "sample post body: " + body + "sample response body: " + externalResponse;
+    return "sample post body: " + body + "\nsample response body: " + externalResponse;
   }
 }
